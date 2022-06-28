5377df0cb3e0cccdf30eb61da435b69709a90874
Khải Bùi Quang <quangkhai123unet@gmail.com>
2022-06-28 15:44:14 +0700

    migration

---
+++ MigrationDemo/entities/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MigrationDemo.entities
+{
+    [Table("article")]
+    internal class Article
+    { 
+        [Key]
+        public int ArticleId { set; get; }
+
+        [StringLength(100)]
+        public string Title { set; get; }
+    }
+}
